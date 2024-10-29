5c477b19aa1608bef4c2fcc225a561768fdf6a07
Vladislav Parfeniuc <v.parfeniuc@hastega.it>
2024-10-29 19:44:57 +0100

    initial commit

---
+++ src/rustmarks/mod.rs
@@ -0,0 +1,259 @@
+use rusqlite::Connection;
+use skim::prelude::*;
+mod sqlite_repository;
+mod ui;
+const DATABASE: &str = "/home/vlad/.config/rustmarks/rustmarks.db";
+
+pub fn main() {
+    let connection = Connection::open(DATABASE).unwrap();
+    let bookmarks = get_bookmark_vec(&connection);
+    let result = ui::open_selection_dialog(&bookmarks);
+    if let Err(e) = result {
+        println!("Error: {}", e);
+        return;
+    }
+    println!("{}", result.unwrap());
+}
+
+pub fn add_bookmark(name: &Option<String>, path: &String, description: &Option<String>) {
+    let bookmark = parse_bookmark(&None, name, path, description);
+    let connection = Connection::open(DATABASE).unwrap();
+    let result = sqlite_repository::add_bookmark(&bookmark, &connection);
+    if let Err(_e) = result {
+        return;
+    }
+    if result.is_ok() {
+        println!("Bookmark added");
+    }
+}
+
+pub fn list_bookmarks() {
+    let connection = Connection::open(DATABASE).unwrap();
+    let bookmarks = get_bookmark_vec(&connection);
+    for bookmark in bookmarks {
+        let str = get_bookmark_string(&bookmark);
+        println!("{}", str);
+    }
+}
+
+pub fn remove_bookmark(id: &i32) {
+    let connection = Connection::open(DATABASE).unwrap();
+    // Check if bookmark exists
+    let result = sqlite_repository::get_bookmark(id, &connection);
+    if let Err(_) = result {
+        println!("error removing bookmark: Bookmark with id {} not found", id);
+        return;
+    }
+    let result = sqlite_repository::remove_bookmark(&id, &connection);
+    if let Err(e) = result {
+        println!("error removing bookmark: {}", e);
+        return;
+    }
+    println!("Bookmark removed");
+}
+
+pub fn update_bookmark(
+    id: &i32,
+    name: &Option<String>,
+    path: &Option<String>,
+    description: &Option<String>,
+) {
+    let connection: Connection = Connection::open(DATABASE).unwrap();
+    let old_bookmark_res = sqlite_repository::get_bookmark(id, &connection);
+    if let Err(e) = old_bookmark_res {
+        println!("Error: {}", e);
+        return;
+    }
+    let old_bookmark = old_bookmark_res.unwrap();
+
+    let new_bookmark_path = match path {
+        Some(path) => Some(path.to_string()),
+        None => old_bookmark.path,
+    };
+
+    // Assemble new bookmark
+    let new_name = match name {
+        Some(name) => Some(name.to_string()),
+        None => old_bookmark.name,
+    };
+
+    let new_description = match description {
+        Some(description) => Some(description.to_string()),
+        None => old_bookmark.description,
+    };
+
+    let new_bookmark = parse_bookmark(
+        &Option::from(id.to_owned()),
+        &new_name,
+        &new_bookmark_path.unwrap(),
+        &new_description,
+    );
+    let result = sqlite_repository::update_bookmark(&id, &new_bookmark, &connection);
+
+    if let Err(e) = result {
+        println!("Error: {}", e);
+        return;
+    }
+    println!("Bookmark updated");
+}
+
+fn get_bookmark_vec(connection: &Connection) -> Vec<Bookmark> {
+    let bookmarks_res = sqlite_repository::list_bookmarks(&connection);
+    let bookmarks = match bookmarks_res {
+        Ok(bookmarks) => bookmarks,
+        Err(e) => {
+            println!("Error: {}", e);
+            return Vec::new();
+        }
+    };
+
+    bookmarks
+}
+
+#[derive(Debug, Clone)]
+struct Bookmark {
+    id: Option<i32>,
+    name: Option<String>,
+    path: Option<String>,
+    description: Option<String>,
+}
+impl SkimItem for Bookmark {
+    fn text(&self) -> Cow<str> {
+        get_bookmark_string_pretty(self).into()
+    }
+    fn output(&self) -> Cow<str> {
+        self.path.as_ref().unwrap().into()
+    }
+    fn preview(&self, _context: PreviewContext) -> ItemPreview {
+        ItemPreview::Text(get_bookmark_preview(self).into())
+    }
+}
+
+fn get_bookmark_string(bookmark: &Bookmark) -> String {
+    let id = match bookmark.id {
+        Some(id) => id.to_string(),
+        None => String::from("None"),
+    };
+
+    let name = match &bookmark.name {
+        Some(name) => name.to_string(),
+        None => String::from("None"),
+    };
+
+    let path = match &bookmark.path {
+        Some(path) => path.to_string(),
+        None => String::from("None"),
+    };
+
+    let description = match &bookmark.description {
+        Some(description) => description.to_string(),
+        None => String::from("None"),
+    };
+
+    let str = format!(
+        "id: {}, name: {}, path: {}, description: {}",
+        id, name, path, description
+    );
+    str
+}
+
+fn get_bookmark_string_pretty(bookmark: &Bookmark) -> String {
+    let type_icon = get_type_icon(bookmark);
+    let name = match &bookmark.name {
+        Some(name) => name.to_string(),
+        None => String::from(""),
+    };
+
+    let path = match &bookmark.path {
+        Some(path) => {
+            if name.is_empty() {
+                path.to_string()
+            } else {
+                format!("")
+            }
+        }
+        None => String::from(""),
+    };
+    let result = format!("{} {} {}", type_icon, name, path);
+    result
+}
+
+fn get_type_icon(bookmark: &Bookmark) -> String {
+    let path = match &bookmark.path {
+        Some(path) => path.to_string(),
+        None => String::from(""),
+    };
+    if path.is_empty() {
+        String::from("❔")
+    } else if std::path::Path::new(&path).is_file() {
+        String::from("📄")
+    } else if std::path::Path::new(&path).is_dir() {
+        String::from("📁")
+    } else {
+        String::from("❔")
+    }
+}
+
+fn get_bookmark_preview(bookmark: &Bookmark) -> String{
+    let type_icon = get_type_icon(bookmark);
+    let path_string = match &bookmark.path {
+        Some(path) => format!("Path: {}",path.to_string()),
+        None => String::from(""),
+    };
+
+    let name_string = match &bookmark.name {
+        Some(name) => format!("{}",name.to_string()),
+        None => String::from(""),
+    };
+
+    let description_string = match &bookmark.description {
+        Some(description) => format!("Description :{}",description.to_string()),
+        None => String::from(""),
+    };
+
+    let eza_output = get_eza_output(bookmark);
+
+    let result = format!(" {} {} \n {} \n {}",type_icon,name_string,path_string,description_string);
+    result
+}
+fn get_eza_output(bookmark: &Bookmark) -> String {
+    let path_string = match &bookmark.path {
+        Some(path) => path.to_string(),
+        None => String::from(""),
+    };
+
+    path_string
+
+}
+fn parse_bookmark(
+    id: &Option<i32>,
+    name: &Option<String>,
+    path: &String,
+    description: &Option<String>,
+) -> Bookmark {
+    let absolute_path = std::fs::canonicalize(path);
+
+    if let Err(e) = absolute_path {
+        println!("Error: {}", e);
+        return Bookmark {
+            id: None,
+            name: None,
+            path: None,
+            description: None,
+        };
+    }
+
+    let path_opt = absolute_path.unwrap();
+
+    let path = match path_opt.to_str() {
+        Some(p) => Some(p.to_string()),
+        None => None,
+    };
+
+    Bookmark {
+        id: id.clone(),
+        name: name.clone(),
+        path: Some(path.unwrap().to_string()),
+        description: description.clone(),
+    }
+}
---
+++ src/rustmarks/ui/mod.rs
@@ -0,0 +1,31 @@
+use super::Bookmark;
+use skim::prelude::*;
+
+pub fn open_selection_dialog(bookmarks: &Vec<Bookmark>) -> Result<String, String> {
+    let options = SkimOptionsBuilder::default()
+        .height(Some("20%"))
+        .multi(false)
+        .preview(Some("inline"))
+        .layout("reverse")
+        .build()
+        .unwrap();
+    let (tx, rx): (SkimItemSender, SkimItemReceiver) = unbounded();
+
+    send_items(bookmarks.to_vec(), tx);
+    let selected_bookmark = Skim::run_with(&options, Some(rx))
+        .map(|out| out.selected_items)
+        .unwrap_or_else(Vec::new);
+
+    let result = selected_bookmark
+        .get(0)
+        .map(|selected_bookmark| selected_bookmark.output().into_owned());
+
+    //FIX: Returning selection on user exit
+    return Ok(result.unwrap_or_default());
+}
+
+fn send_items(items: Vec<Bookmark>, tx: SkimItemSender) {
+    for item in items {
+        tx.send(Arc::new(item)).unwrap();
+    }
+}
