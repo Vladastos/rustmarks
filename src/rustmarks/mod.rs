cc48e96dcc090be053fa0c7076f6a26531a215a1
Vladislav Parfeniuc <v.parfeniuc@hastega.it>
2024-10-30 23:43:30 +0100

    refactored code

--- src/rustmarks/mod.rs
+++ src/rustmarks/mod.rs
@@ -1,6 +1,7 @@
 use rusqlite::Connection;
 use skim::prelude::*;
 use std::path::PathBuf;
+use std::path::Path;
 mod sqlite_repository;
 mod ui;
 const DATABASE: &str = "/home/vlad/.config/rustmarks/rustmarks.db";
@@ -32,7 +33,7 @@ pub fn list_bookmarks() {
     let connection = Connection::open(DATABASE).unwrap();
     let bookmarks = get_bookmark_vec(&connection);
     for bookmark in bookmarks {
-        let str = get_bookmark_string(&bookmark);
+        let str = ui::get_bookmark_string(&bookmark);
         println!("{}", str);
     }
 }
@@ -120,149 +121,16 @@ struct Bookmark {
 }
 impl SkimItem for Bookmark {
     fn text(&self) -> Cow<str> {
-        get_bookmark_string_pretty(self).into()
+        ui::get_bookmark_string_pretty(self).into()
     }
     fn output(&self) -> Cow<str> {
         self.path.as_ref().unwrap().into()
     }
     fn preview(&self, _context: PreviewContext) -> ItemPreview {
-        ItemPreview::Text(get_bookmark_preview(self).into())
+        ItemPreview::Text(ui::get_bookmark_preview(self).into())
     }
 }
 
-fn get_bookmark_string(bookmark: &Bookmark) -> String {
-    let id = match bookmark.id {
-        Some(id) => id.to_string(),
-        None => String::from("None"),
-    };
-
-    let name = match &bookmark.name {
-        Some(name) => name.to_string(),
-        None => String::from("None"),
-    };
-
-    let path = match &bookmark.path {
-        Some(path) => path.to_string(),
-        None => String::from("None"),
-    };
-
-    let description = match &bookmark.description {
-        Some(description) => description.to_string(),
-        None => String::from("None"),
-    };
-
-    let str = format!(
-        "id: {}, name: {}, path: {}, description: {}",
-        id, name, path, description
-    );
-    str
-}
-
-fn get_bookmark_string_pretty(bookmark: &Bookmark) -> String {
-    let type_icon = get_type_icon(&bookmark.path);
-    let name = match &bookmark.name {
-        Some(name) => name.to_string(),
-        None => String::from(""),
-    };
-
-    let path = match &bookmark.path {
-        Some(path) => {
-            if name.is_empty() {
-                path.to_string()
-            } else {
-                format!("")
-            }
-        }
-        None => String::from(""),
-    };
-    let result = format!("{} {} {}", type_icon, name, path);
-    result
-}
-
-fn get_type_icon(path: &Option<String>) -> String {
-    let path = match &path {
-        Some(path) => path.to_string(),
-        None => String::from(""),
-    };
-    if path.is_empty() {
-        String::from("❔")
-    } else if std::path::Path::new(&path).is_file() {
-        String::from("📄")
-    } else if std::path::Path::new(&path).is_dir() {
-        String::from("📁")
-    } else {
-        String::from("❔")
-    }
-}
-
-fn get_bookmark_preview(bookmark: &Bookmark) -> String {
-    let type_icon = get_type_icon(&bookmark.path);
-    let path_string = match &bookmark.path {
-        Some(_) => format!(""),
-        None => String::from(""),
-    };
-
-    let name_string = match &bookmark.name {
-        Some(name) => format!("{} \n", name.to_string()),
-        None => String::from(""),
-    };
-
-    let description_string = match &bookmark.description {
-        Some(description) => format!("\n {}", description.to_string()),
-        None => String::from("\n"),
-    };
-    let separator_string = String::from("\n-----------------------------------------");
-    let preview_content = get_preview_content(bookmark);
-
-    let result = format!(
-        " {} {}{}{}{}{}",
-        type_icon, name_string, description_string, path_string, separator_string, preview_content
-    );
-    result
-}
-fn get_preview_content(bookmark: &Bookmark) -> String {
-    let path_string = match &bookmark.path {
-        Some(path) => path.to_string(),
-        None => String::from(""),
-    };
-    let mut result = String::from("");
-    //check if path is a file
-    if std::path::Path::new(&path_string).is_file() {
-        let content = match std::fs::read_to_string(&path_string) {
-            Ok(content) => content,
-            Err(_) => String::from(""),
-        };
-
-        if !content.is_empty() {
-            result = format!("\n{}", content); // add content
-        }
-    } else if std::path::Path::new(&path_string).is_dir() {
-        let eza_dir = eza::fs::Dir::read_dir(PathBuf::from(path_string.clone()));
-        if let Ok(eza_dir) = eza_dir {
-            let files = eza_dir.files(eza::fs::DotFilter::JustFiles, None, true, true, false);
-            // TODO: find a better way to get the last file
-            let last_file = eza_dir.files(eza::fs::DotFilter::JustFiles, None, true, true, false).last().unwrap();
-
-            // Add folder name to the result
-            let folder_name =eza_dir.path.file_name().unwrap().to_str().unwrap().to_string();
-            result = format!("\n{} {}", "📁",folder_name);
-            
-            // TODO: Sort directories first
-            for file in files {
-                let type_icon =
-                    get_type_icon(&Option::from(file.path.to_str().unwrap().to_string()));
-                let filename = file.name;
-                let tree_char = if last_file.absolute_path() == Some(&file.path) {
-                    "└─"
-                } else {
-                    "├─"
-                };
-                result = format!("{}\n{}{} {}", result, tree_char, type_icon, filename);
-            }
-        }
-    }
-    result
-}
 fn parse_bookmark(
     id: &Option<i32>,
     name: &Option<String>,
--- src/rustmarks/ui/mod.rs
+++ src/rustmarks/ui/mod.rs
@@ -1,5 +1,7 @@
 use super::Bookmark;
 use skim::prelude::*;
+use std::path::PathBuf;
+use std::path::Path;
 
 pub fn open_selection_dialog(bookmarks: &Vec<Bookmark>) -> Result<String, String> {
     let options = SkimOptionsBuilder::default()
@@ -11,9 +13,15 @@ pub fn open_selection_dialog(bookmarks: &Vec<Bookmark>) -> Result<String, String
         .layout("reverse")
         .build()
         .unwrap();
+
+    //Create channel
     let (tx, rx): (SkimItemSender, SkimItemReceiver) = unbounded();
 
-    send_items(bookmarks.to_vec(), tx);
+    // Send all bookmarks to skim through channel
+    for item in bookmarks.to_vec() {
+        tx.send(Arc::new(item)).unwrap();
+    }
+
     let selected_bookmark = Skim::run_with(&options, Some(rx))
         .map(|out| match out.final_key {
             Key::Enter => out.selected_items,
@@ -29,12 +37,6 @@ pub fn open_selection_dialog(bookmarks: &Vec<Bookmark>) -> Result<String, String
     return Ok(result.unwrap_or_default());
 }
 
-fn send_items(items: Vec<Bookmark>, tx: SkimItemSender) {
-    for item in items {
-        tx.send(Arc::new(item)).unwrap();
-    }
-}
-
 fn delete_selected_items(items: Vec<Arc<dyn SkimItem>>) -> Vec<Arc<dyn SkimItem>> {
     
     for item in items.clone() {
@@ -42,4 +44,173 @@ fn delete_selected_items(items: Vec<Arc<dyn SkimItem>>) -> Vec<Arc<dyn SkimItem>
         super::remove_bookmark(&bookmark.id.unwrap());
     }
     items
+}
+
+
+
+// Preview
+
+pub fn get_bookmark_preview(bookmark: &Bookmark) -> String {
+    let type_icon = get_type_icon(&bookmark.path);
+    let path_string = match &bookmark.path {
+        Some(_) => format!(""),
+        None => String::from(""),
+    };
+
+    let name_string = match &bookmark.name {
+        Some(name) => format!("{} \n", name.to_string()),
+        None => String::from(""),
+    };
+
+    let description_string = match &bookmark.description {
+        Some(description) => format!("\n {}", description.to_string()),
+        None => String::from("\n"),
+    };
+    let separator_string = String::from("\n-----------------------------------------");
+    let preview_content = get_preview_content(bookmark);
+
+    let result = format!(
+        " {} {}{}{}{}{}",
+        type_icon, name_string, description_string, path_string, separator_string, preview_content
+    );
+    result
+}
+
+fn get_preview_content(bookmark: &Bookmark) -> String {
+    let path_string = match &bookmark.path {
+        Some(path) => path.to_string(),
+        None => String::from(""),
+    };
+
+    if path_string.is_empty() {
+        return String::from("");
+    }
+    let path = Path::new(&path_string);
+
+    let result = match path {
+        path if path.is_file() => get_file_preview(&path_string),
+        path if path.is_dir() => get_dir_preview(&path_string),
+        _ => get_unknown_preview(&path_string),
+        
+    };
+    result
+}
+
+fn get_file_preview(path_string: &String) -> String {
+    let mut result = String::from("");
+    let content = match std::fs::read_to_string(&path_string) {
+        Ok(content) => content,
+        Err(_) => String::from(""),
+    };
+
+    if !content.is_empty() {
+        result = format!("\n{}", content); // add content
+    }
+    result
+}
+
+fn get_dir_preview(path_string: &String) -> String {
+    let mut result = String::from("");
+    let eza_dir = eza::fs::Dir::read_dir(PathBuf::from(path_string.clone()));
+        if let Ok(eza_dir) = eza_dir {
+            let files = eza_dir.files(eza::fs::DotFilter::JustFiles, None, true, true, false);
+            // TODO: find a better way to get the last file
+            let last_file = eza_dir.files(eza::fs::DotFilter::JustFiles, None, true, true, false).last().unwrap();
+
+            // Add folder name to the result
+            let folder_name =eza_dir.path.file_name().unwrap().to_str().unwrap().to_string();
+            result = format!("\n{} {}", "📁",folder_name);
+            
+            // TODO: Sort directories first
+
+            let files_vec = files.into_iter().collect::<Vec<_>>();
+            for file in files_vec {
+                let type_icon =
+                    get_type_icon(&Option::from(file.path.to_str().unwrap().to_string()));
+                let filename = file.name;
+                let tree_char = if last_file.absolute_path() == Some(&file.path) {
+                    "└─"
+                } else {
+                    "├─"
+                };
+                result = format!("{}\n{}{} {}", result, tree_char, type_icon, filename);
+            }
+        }
+
+    result
+}
+
+fn get_unknown_preview(path_string: &String) -> String {
+    format!("Could not preview: {}", path_string)
+}
+
+// Bookmark utils
+
+pub fn get_bookmark_string(bookmark: &Bookmark) -> String {
+    let id = match bookmark.id {
+        Some(id) => id.to_string(),
+        None => String::from("None"),
+    };
+
+    let name = match &bookmark.name {
+        Some(name) => name.to_string(),
+        None => String::from("None"),
+    };
+
+    let path = match &bookmark.path {
+        Some(path) => path.to_string(),
+        None => String::from("None"),
+    };
+
+    let description = match &bookmark.description {
+        Some(description) => description.to_string(),
+        None => String::from("None"),
+    };
+
+    let str = format!(
+        "id: {}, name: {}, path: {}, description: {}",
+        id, name, path, description
+    );
+    str
+}
+
+pub fn get_bookmark_string_pretty(bookmark: &Bookmark) -> String {
+    let type_icon = get_type_icon(&bookmark.path);
+    let name = match &bookmark.name {
+        Some(name) => name.to_string(),
+        None => String::from(""),
+    };
+
+    let path = match &bookmark.path {
+        Some(path) => {
+            if name.is_empty() {
+                path.to_string()
+            } else {
+                format!("")
+            }
+        }
+        None => String::from(""),
+    };
+    let result = format!("{} {} {}", type_icon, name, path);
+    result
+}
+
+
+
+// UI utils
+
+pub fn get_type_icon(path: &Option<String>) -> String {
+    let path = match &path {
+        Some(path) => path.to_string(),
+        None => String::from(""),
+    };
+    if path.is_empty() {
+        String::from("❔")
+    } else if std::path::Path::new(&path).is_file() {
+        String::from("📄")
+    } else if std::path::Path::new(&path).is_dir() {
+        String::from("📁")
+    } else {
+        String::from("❔")
+    }
 }
