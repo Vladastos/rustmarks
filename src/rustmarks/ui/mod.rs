0c5d39b297e6cbab61ab270ead9d954889849156
Vladislav Parfeniuc <v.parfeniuc@hastega.it>
2024-10-30 12:48:00 +0100

    Added option to delete bookmark from the interface

--- src/rustmarks/ui/mod.rs
+++ src/rustmarks/ui/mod.rs
@@ -5,6 +5,8 @@ pub fn open_selection_dialog(bookmarks: &Vec<Bookmark>) -> Result<String, String
     let options = SkimOptionsBuilder::default()
         .height(Some("50%"))
         .multi(false)
+        .header(Some("Enter: select, Ctrl-x: delete, Ctrl-c: exit"))
+        .bind(vec!["ctrl-x:accept"])
         .preview(Some("inline"))
         .layout("reverse")
         .build()
@@ -13,14 +15,17 @@ pub fn open_selection_dialog(bookmarks: &Vec<Bookmark>) -> Result<String, String
 
     send_items(bookmarks.to_vec(), tx);
     let selected_bookmark = Skim::run_with(&options, Some(rx))
-        .map(|out| out.selected_items)
+        .map(|out| match out.final_key {
+            Key::Enter => out.selected_items,
+            Key::Ctrl('x') => delete_selected_items(out.selected_items),
+            _ => Vec::new(),
+        })
         .unwrap_or_else(Vec::new);
 
     let result = selected_bookmark
         .get(0)
         .map(|selected_bookmark| selected_bookmark.output().into_owned());
 
-    //FIX: Returning selection on user exit
     return Ok(result.unwrap_or_default());
 }
 
@@ -28,4 +33,13 @@ fn send_items(items: Vec<Bookmark>, tx: SkimItemSender) {
     for item in items {
         tx.send(Arc::new(item)).unwrap();
     }
+}
+
+fn delete_selected_items(items: Vec<Arc<dyn SkimItem>>) -> Vec<Arc<dyn SkimItem>> {
+    
+    for item in items.clone() {
+        let bookmark = (*item).as_any().downcast_ref::<Bookmark>().expect("Something wrong with downcast");
+        super::remove_bookmark(&bookmark.id.unwrap());
+    }
+    items
 }
